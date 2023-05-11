cc53807245bb9a2b9d67c9a0ceb252aff3ce1ee9
Ketki Panse <ketki.skpanse@gmail.com>
2023-05-11 10:19:37 +0530

    Fixing gitignore and resolving build errors

--- components/ExpCard.tsx
+++ components/ExpCard.tsx
@@ -11,11 +11,11 @@ export default function ExpCard({date, title, position, children, tags}: ExpCard
     return(
         <div className='hover:ml-2 hover:from-red-400 hover:to-red-200 hover:bg-gradient-to-tr rounded-md p-6 bg-opacity-5 bg-red-500'>
             <div className="text-xs text-orange-800 font-semibold">{date}</div>
-            <div className="text-sm font-bold" role="heading">{title}</div>
-            <div className="text-sm" role="heading">{position}</div>
+            <div className="text-sm font-bold" role="heading" aria-level={1}>{title}</div>
+            <div className="text-sm" role="heading" aria-level={2}>{position}</div>
             <div className="text-sm text-justify pt-1">{children}</div>
             <div className="inline-flex text-xs text-center space-x-1">
-                {tags.map(value => (<div className="rounded-full bg-gradient-to-r from-red-600 to-red-400 px-1 align-middle">{value}</div>))}
+                {tags.map(value => (<div className="rounded-full bg-gradient-to-r from-red-600 to-red-400 px-1 align-middle" key={value}>{value}</div>))}
             </div>
         </div>
     )
